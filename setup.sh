64cdea010cb0b28aef5d8eac52fd7cb448304c10
Han Sangwon <han_sangwon@cyberagent.co.jp>
2018-04-24 18:25:09 +0900

    new

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd $HOME
+
+ln -s dotfiles/.aliases .aliases
+ln -s dotfiles/.bash_profile .bash_profile
+ln -s dotfiles/.bash_prompt .bash_prompt
+ln -s dotfiles/.bashrc .bashrc
+ln -s dotfiles/.gitconfig .gitconfig
+ln -s dotfiles/.inputrc .inputrc
+ln -s dotfiles/ssh/.ssh .ssh
+ln -s dotfiles/.tmux .tmux
+ln -s dotfiles/.tmux.conf .tmux.conf
+ln -s dotfiles/.vim .vim
+ln -s dotfiles/.vimrc .vimrc
